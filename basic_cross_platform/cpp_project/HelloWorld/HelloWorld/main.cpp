44c407d7b0e217429d08639afafacf038412979b
Matt Chowning <mchowning@gmail.com>
2017-01-02 16:42:48 -0500

    Basic functioning Xcode C++ project

---
+++ basic_cross_platform/cpp_project/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Matt Chowning on 1/2/17.
+//  Copyright © 2017 Matt Chowning. All rights reserved.
+//
+
+#include <iostream>
+#include "hello_world_impl.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    helloworld::HelloWorldImpl hw = helloworld::HelloWorldImpl();
+    
+    std::string myString = hw.get_hello_world();
+    
+    std::cout << myString << "\n";
+    
+    return 0;
+}
